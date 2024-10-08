829fef7ae4224edd25ee46cd7cb60cb93a6e7ac6
parth <parth@PARTH14>
2024-10-07 21:55:07 -0400

    change the datatype of the variable

--- DigicalulatorAPI/Services/CalculatorService.cs
+++ DigicalulatorAPI/Services/CalculatorService.cs
@@ -4,7 +4,7 @@ namespace DigicalculatorAPI.Services
 {
     public class CalculatorService : ICalculatorService
     {
-        public double Add(double a, double b) => a + b;
+        public double Add(decimal a, decimal b) => a + b;
 
         public double Subtract(double a, double b) => a - b;
 

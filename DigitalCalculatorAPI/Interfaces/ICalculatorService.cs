cc7080bca90a0354737e834b2248f7e8dfefe596
parth <parth@PARTH14>
2024-10-08 15:12:46 -0400

    second commit

---
+++ DigitalCalculatorAPI/Interfaces/ICalculatorService.cs
@@ -0,0 +1,10 @@
+﻿namespace DigitalCalculatorApi.Interfaces
+{
+    public interface ICalculatorService
+    {
+        double Add(double a, double b);
+        double Subtract(double a, double b);
+        double Multiply(double a, double b);
+        double Divide(double a, double b);
+    }
+}
